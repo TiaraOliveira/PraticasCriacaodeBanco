1d1a63fac93fc4a13962b4d28c495f4b0ec9717e
TiaraOliveira <tiaragomes@yahoo.com.br>
2022-07-29 20:57:34 -0300

    First Commit

---
+++ queries.sql
@@ -0,0 +1,72 @@
+CREATE TABLE "customers"(
+"id" SERIAL PRIMARY KEY,
+"fullName" TEXT NOT NULL,
+"cpf" varchar(11) NOT NULL UNIQUE,
+"email" text NOT NULL,
+"password" text NOT NULL
+)
+
+SELECT * FROM customers;
+
+CREATE TABLE states(
+"id" SERIAL PRIMARY KEY,
+"name" TEXT NOT NULL)
+
+SELECT * FROM states;
+
+CREATE table cities(
+	"id" SERIAL PRIMARY KEY,
+	"name" TEXT,
+	"stateId" INTEGER NOT NULL REFERENCES "states"("id") 
+)
+SELECT * FROM cities;
+
+CREATE TABLE "customerAddresses"(
+"id" SERIAL PRIMARY KEY,
+"customerId" INTEGER NOT NULL REFERENCES "customers"("id") UNIQUE,
+"street" TEXT NOT NULL,
+"number" INTEGER NOT NULL,
+complement TEXT,
+"postalCode" VARCHAR(8),
+"cityId" INTEGER NOT NULL REFERENCES "cities"("id") 
+)
+
+SELECT * FROM "customerAddresses";
+
+CREATE TABLE "customerPhones"(
+"id" SERIAL PRIMARY KEY,
+"customerId" INTEGER NOT NULL REFERENCES "customers"("id"),
+"number" VARCHAR (11) NOT NULL,
+"type" TEXT
+)
+
+
+CREATE TABLE "bankAccount"(
+"id" SERIAL PRIMARY KEY,
+"customerId" INTEGER NOT NULL REFERENCES "customers"("id"),
+"accountNumber" VARCHAR(30),
+"agency" VARCHAR(6),
+"opendate" date,
+"closeDate" date
+)
+
+CREATE TABLE transactions(
+"id" SERIAL PRIMARY KEY,
+"bankAccountId" INTEGER NOT NULL REFERENCES "bankAccount"("id"),
+"amount" REAL NOT NULL DEFAULT 0,
+"type" TEXT NOT NULL DEFAULT 'Conta Corrente',
+"time" TIME, 
+description TEXT,
+"cancelled" BOOLEAN DEFAULT false
+)
+
+CREATE TABLE "creditCards"(
+"id" SERIAL PRIMARY KEY,
+"bankAccountId" INTEGER NOT NULL REFERENCES "bankAccount"("id"),
+"name" TEXT NOT NULL,
+"securityCode" VARCHAR(3) NOT NULL,
+"expirationMonth" VARCHAR(2) NOT NULL,
+"expirationYear" VARCHAR(4) NOT NULL,
+"password" VARCHAR(6) NOT NULL,
+"limit" REAL NOT NULL DEFAULT 1000
+)
